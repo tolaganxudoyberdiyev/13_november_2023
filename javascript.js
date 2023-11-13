5255d810a5772189c1e0126f7d2144f8aa49a446
tolaganxudoyberdiyev <tolaganxudoyberdiyev2023@gamil.com>
2023-11-14 01:08:54 +0500

    jqhf

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+function icon() {
+    let bars = document.getElementById("my__right");
+    if (bars.style.display === "block") {
+      bars.style.display = "none";
+    } else {
+      bars.style.display = "block";
+    }
+  }
